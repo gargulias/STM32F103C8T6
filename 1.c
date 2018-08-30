45b7bc0febe2f727aa70972765b45b50511cb1db
gargulias <42836548+gargulias@users.noreply.github.com>
2018-08-30 14:42:26 +0300

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,32 @@
+﻿/*
+ * first_project_c.c
+ *
+ *  Created on: 18 окт. 2017 г.
+ *      Author: admin
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void selection_sort(int array[], int size) {
+	int temp, current, j;
+
+	for(current = 0; current < size; current++)
+		for(j = current + 1; j < size; j++)
+			if(array[current] < array[j]) {
+				temp = array[current];
+				array[current] = array[j];
+				array[j] = temp;
+			}
+}
+int main(void) {
+	int values[30], i;
+
+	for(i = 0; i < 30; i++)
+		values[i] = rand() % 100;
+
+	selection_sort(values, 30);
+
+	for(i = 0; i < 30; i++)
+		printf("%d ", values[i]);
+}
